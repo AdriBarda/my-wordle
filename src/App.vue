8984aa42f9f679fd3af1f0c3a5d1c67a3c0b29e7
AdriBarda <adri.bardaji@icloud.com>
2025-11-24 11:27:29 +0100

    trying out vitest

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import WordleBoard from './components/WordleBoard.vue'
+
+const msg = ref('Hello Wordle!!!')
+</script>
 
 <template>
   <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <WordleBoard :msg="msg" />
 </template>
---
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import WordleBoard from '../WordleBoard.vue'
+
+describe('WordleBoard', () => {
+  it('renders properly', () => {
+    const wrapper = mount(WordleBoard, { props: { msg: 'Hello Wordle!!!' } })
+    expect(wrapper.text()).toContain('Hello Wordle!!!')
+  })
+})
