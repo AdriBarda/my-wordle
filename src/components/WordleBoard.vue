e5bddd13ad83879eb0a88fde00a71c4b3df40e77
AdriBarda <adri.bardaji@icloud.com>
2025-11-25 12:49:46 +0100

    extract conditional into computed prop

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { DEFEAT_MESSAGE, MAX_GUESSES_COUNT, VICTORY_MESSAGE } from '@/settings'
 import englishWords from '@/englishWordsWith5Letters.json'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import GuessInput from './GuessInput.vue'
 
-defineProps({
+const props = defineProps({
   wordOfTheDay: {
     type: String,
     required: true,
@@ -13,6 +13,12 @@ defineProps({
 })
 
 const guessesSubmitted = ref<string[]>([])
+
+const isGameOver = computed(
+  () =>
+    guessesSubmitted.value.length === MAX_GUESSES_COUNT ||
+    guessesSubmitted.value.includes(props.wordOfTheDay),
+)
 </script>
 
 <template>
@@ -20,7 +26,7 @@ const guessesSubmitted = ref<string[]>([])
     :class="[guessesSubmitted ? 'block' : 'hidden', 'absolute w-screen h-screen bg-black/10']"
   ></div>
   <p
-    v-if="guessesSubmitted.length === MAX_GUESSES_COUNT || guessesSubmitted.includes(wordOfTheDay)"
+    v-if="isGameOver"
     v-text="guessesSubmitted.includes(wordOfTheDay) ? VICTORY_MESSAGE : DEFEAT_MESSAGE"
     class="absolute flex items-center justify-center text-4xl text-pretty text-center text-white bg-black rounded-3xl shadow-lg w-[750px] h-96 top-1/2 left-1/2 -translate-1/2 z-10"
   />
