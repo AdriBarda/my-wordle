630ed5d115aafc9a42f8a2d2f376874553629123
AdriBarda <adri.bardaji@icloud.com>
2025-11-26 13:04:08 +0100

    display unknown feedback upon successful guess submition

--- src/components/GuessDisplayer.vue
+++ src/components/GuessDisplayer.vue
@@ -13,6 +13,7 @@ const { shouldFlip = false } = defineProps<{
       v-for="(char, index) in guess?.padEnd(WORD_SIZE, ' ') || new Array(5)"
       :key="index"
       class="w-20 h-20 perspective-midrange"
+      :data-letter-feedback="shouldFlip ? 'unknown' : null"
     >
       <div
         class="inner w-full h-full relative transform-3d"
